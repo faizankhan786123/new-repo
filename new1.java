f32008b292023ed729accc8d171d6126daed13d8
faizankhan786123 <faizankh786123@gmail.com>
2025-05-04 23:12:31 +0530

    Add files via upload

---
+++ new1.java
@@ -0,0 +1,12 @@
+public class JsonInputExample {
+    public static void main(String[] args) {
+        String jsonInput = "{\n" +
+                           "  \"properties\": {\n" +
+                           "    \"WI_No\": \"NBTL-0000002148-Process\"\n" +
+                           "  }\n" +
+                           "}";
+        
+        System.out.println(jsonInput);
+    }
+}
+ 
