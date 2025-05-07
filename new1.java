34a9c9f54be6f89c8e874984f78c7d625dba73d0
faizankhan786123 <faizankh786123@gmail.com>
2025-05-07 12:01:01 +0530

    Update new1.java

--- new1.java
+++ new1.java
@@ -1,18 +1,20 @@
 if (SMSResponse.equalsIgnoreCase(SuccessResponseCode)) {
-						updateStatus("'" + SuccessCode + "'", whereCondition);
-						ResponseCode = SuccessCode;
-						loggerManager.insertSMSLog(sWORKITEM_NO, sInputXML, SMSResponse, ResponseCode,sessionID);
+    mLogger.info("SMS sent successfully for WorkItem: " + sWORKITEM_NO);
+    mLogger.info("Updating status to SuccessCode: " + SuccessCode + " for condition: " + whereCondition);
+    updateStatus("'" + SuccessCode + "'", whereCondition);
+    
+    ResponseCode = SuccessCode;
 
-					} else {
-						updateStatus("'" + FailCode + "'", whereCondition);
-						ResponseCode = FailCode;
-						loggerManager.insertSMSLog(sWORKITEM_NO, sInputXML, SMSResponse, ResponseCode,sessionID);
+    mLogger.info("Inserting SMS log with SuccessCode...");
+    loggerManager.insertSMSLog(sWORKITEM_NO, sInputXML, SMSResponse, ResponseCode, sessionID);
 
-					}
+} else {
+    mLogger.info("SMS failed for WorkItem: " + sWORKITEM_NO);
+    mLogger.info("Updating status to FailCode: " + FailCode + " for condition: " + whereCondition);
+    updateStatus("'" + FailCode + "'", whereCondition);
+    
+    ResponseCode = FailCode;
 
-
-
-				mLogger.info("Complete ArrayList Values: " + ArrLstTableValues.toString());
-
-
-
+    mLogger.info("Inserting SMS log with FailCode...");
+    loggerManager.insertSMSLog(sWORKITEM_NO, sInputXML, SMSResponse, ResponseCode, sessionID);
+}
