2615424800314e55fab766f7015049ab3f074b2f
faizankhan786123 <faizankh786123@gmail.com>
2025-05-08 11:36:13 +0400

    Update new1.java

--- new1.java
+++ new1.java
@@ -1,20 +1,78 @@
-if (SMSResponse.equalsIgnoreCase(SuccessResponseCode)) {
-    mLogger.info("SMS sent successfully for WorkItem: " + sWORKITEM_NO);
-    mLogger.info("Updating status to SuccessCode: " + SuccessCode + " for condition: " + whereCondition);
-    updateStatus("'" + SuccessCode + "'", whereCondition);
-    
-    ResponseCode = SuccessCode;
+private static String postAPI() {
+        String baseUrl = https://sms-interceptor-uat.rakbanktst.ae/sms-receiver/sms/3/text/advanced;
 
-    mLogger.info("Inserting SMS log with SuccessCode...");
-    loggerManager.insertSMSLog(sWORKITEM_NO, sInputXML, SMSResponse, ResponseCode, sessionID);
+        try {
+            // POST Request
+            URL postUrl = new URL(baseUrl);
+            HttpURLConnection postConnection = (HttpURLConnection) postUrl.openConnection();
+            Digital_PL.mLogger.debug("After URL connection:");
 
-} else {
-    mLogger.info("SMS failed for WorkItem: " + sWORKITEM_NO);
-    mLogger.info("Updating status to FailCode: " + FailCode + " for condition: " + whereCondition);
-    updateStatus("'" + FailCode + "'", whereCondition);
-    
-    ResponseCode = FailCode;
+            // Set request method and headers
+            postConnection.setRequestMethod("POST");
+            postConnection.setRequestProperty("Content-Type", "application/json");
+            postConnection.setRequestProperty("source", "application/json");
+            postConnection.setDoOutput(true);
 
-    mLogger.info("Inserting SMS log with FailCode...");
-    loggerManager.insertSMSLog(sWORKITEM_NO, sInputXML, SMSResponse, ResponseCode, sessionID);
+            // JSON request body for creating a new post
+            String jsonBody = "[\r\n"
+                               + "                  {\r\n"
+                               + "                    \"sender\": \"RAKBANK\",\r\n"
+                               + "                    \"destinations\": [\r\n"
+                               + "                      {\r\n"
+                               + "                        \"to\": \"971586660000\"\r\n"
+                               + "                      }\r\n"
+                               + "                    ],\r\n"
+                               + "                    \"content\": {\r\n"
+                               + "                      \"text\": \"This is a sample message\"\r\n"
+                               + "                    }\r\n"
+                               + "                  }\r\n"
+                               + "                ]";
+
+            Digital_PL.mLogger.debug("After JSON Request Construction" + jsonBody);
+
+            try (DataOutputStream outputStream = new DataOutputStream(postConnection.getOutputStream())) {
+                outputStream.writeBytes(jsonBody);
+                outputStream.flush();
+            }
+
+            Digital_PL.mLogger.debug("After Output Stream flush");
+
+            // Read POST response
+            Digital_PL.mLogger.debug(postConnection.getResponseCode());
+            int postResponseCode = postConnection.getResponseCode();
+            StringBuilder postResponse = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(postConnection.getInputStream()))) {
+                Digital_PL.mLogger.debug("Inside Response from Input Stream");
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null) {
+                    postResponse.append(inputLine);
+                }
+            }
+
+            // Print POST response
+            Digital_PL.mLogger.debug("POST Request Response:");
+            Digital_PL.mLogger.debug("Status Code: " + postResponseCode);
+            Digital_PL.mLogger.debug("Response Body: " + postResponse.toString());
+
+            // Close connections
+            postConnection.disconnect();
+            return postResponseCode + "~" + postResponse.toString();
+
+        } catch (Exception e) {
+            Digital_PL.mLogger.debug("Error: " + e.getMessage());
+            e.printStackTrace();
+            return "Exception Occured in: " + e.getMessage();
+        }
+    }
+	
+	
+	
+	https://sms-interceptor-dev.rakbanktst.ae/sms-receiver/peopleevents/2/persons/2695757.2695757/definitions/BPMNBTLAP/events
+
+
+{
+                "properties": {
+                  "WI_No": "NBTL-0000002148-Process"
+                }
 }
+
